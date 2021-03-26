0c59e44b25ada8c5403351a9834c2fa1afb8a845
Yan Wittmann <order@yanwittmann.de>
2021-03-26 20:35:44 +0100

    Added notifications

--- src/yanwittmann/PerformanceTest.java
+++ src/yanwittmann/PerformanceTest.java
@@ -8,6 +8,9 @@ import java.util.Arrays;
  * This class has been written by <a href="http://yanwittmann.de">Yan Wittmann</a>.
  */
 public abstract class PerformanceTest {
+
+    private final static long DEFAULT_VALUE = -1;
+
     private final int repeatCount;
     private ArrayList<Long> results = null;
 
@@ -30,40 +33,59 @@ public abstract class PerformanceTest {
         return this;
     }
 
-    public long getResult() {
+    /**
+     * Override this method to implement your own test.
+     */
+    public abstract void perform();
+
+    public PerformanceTest removeFirst() {
+        if (results.size() > 0)
+            results.remove(0);
+        return this;
+    }
+
+    public PerformanceTest removeLast() {
+        if (results.size() > 0)
+            results.remove(results.size() - 1);
+        return this;
+    }
+
+    public long getTotalResult() {
+        if (results.size() == 0) return DEFAULT_VALUE;
         return results.stream().mapToLong(result -> result).sum();
     }
 
     public long getAverageResult() {
+        if (results.size() == 0) return DEFAULT_VALUE;
         return results.stream().mapToLong(result -> result).sum() / repeatCount;
     }
 
     public long getMaxResult() {
-        if (results.size() > 0)
-            return results.stream().mapToLong(result -> result).max().getAsLong();
-        return 0;
+        if (results.size() == 0) return DEFAULT_VALUE;
+        return results.stream().mapToLong(result -> result).max().getAsLong();
     }
 
     public long getMinResult() {
-        if (results.size() > 0)
-            return results.stream().mapToLong(result -> result).min().getAsLong();
-        return 0;
+        if (results.size() == 0) return DEFAULT_VALUE;
+        return results.stream().mapToLong(result -> result).min().getAsLong();
     }
 
     public void print() {
-        System.out.println(toString());
+        if (results.size() == 0)
+            System.out.println("Make sure to run the PerformanceTest before getting the results!");
+        else System.out.println(toString());
     }
 
     @Override
     public String toString() {
-        ArrayList<ArrayList<String>> rows = new ArrayList<>();
-        rows.add(new ArrayList<>(Arrays.asList("all", "" + Arrays.toString(results.toArray(new Long[0])))));
-        rows.add(new ArrayList<>(Arrays.asList("total", "" + getResult())));
-        rows.add(new ArrayList<>(Arrays.asList("max", "" + getMaxResult())));
-        rows.add(new ArrayList<>(Arrays.asList("min", "" + getMinResult())));
-        rows.add(new ArrayList<>(Arrays.asList("average", "" + getAverageResult())));
-        return "PerformanceTest results in ms:\n" + GeneralUtils.formatAsTable(rows);
+        if (results.size() > 0) {
+            ArrayList<ArrayList<String>> rows = new ArrayList<>();
+            rows.add(new ArrayList<>(Arrays.asList("all", "" + Arrays.toString(results.toArray(new Long[0])))));
+            rows.add(new ArrayList<>(Arrays.asList("total", "" + getTotalResult())));
+            rows.add(new ArrayList<>(Arrays.asList("max", "" + getMaxResult())));
+            rows.add(new ArrayList<>(Arrays.asList("min", "" + getMinResult())));
+            rows.add(new ArrayList<>(Arrays.asList("average", "" + getAverageResult())));
+            return "PerformanceTest results in ms:\n" + GeneralUtils.formatAsTable(rows);
+        } else return "Make sure to run the PerformanceTest before getting the results!";
     }
-
-    public abstract void perform();
 }
--- src/yanwittmann/Testing.java
+++ src/yanwittmann/Testing.java
@@ -1,13 +1,23 @@
 package yanwittmann;
 
+import yanwittmann.notification.BlurNotification;
+
 import java.io.IOException;
 import java.util.Map;
 
 public class Testing {
 
-    public static void main(String[] args) throws IOException {
-        File file = new File("D:\\files\\create\\programming\\projects\\test.php");
-        file.readToArrayList().forEach(System.out::println);
+    public static void main(String[] args) {
+    }
+
+    public static void notificationTest() {
+        String[] options = {"heel", "page", "service", "awful", "ban", "default", "shatter", "candle", "comprehensive", "offset", "classroom", "legend", "simplicity", "prevent", "tablet", "welcome", "carriage", "village", "fist", "vat", "know"};
+        new PerformanceTest(100) {
+            @Override
+            public void perform() {
+                new BlurNotification(options[GeneralUtils.randomNumber(0, options.length - 1)]);
+            }
+        }.start().removeFirst().print();
     }
 
     public static void translateTest() {
