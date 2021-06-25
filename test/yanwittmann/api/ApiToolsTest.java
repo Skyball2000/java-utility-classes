38590ffe43ce232a7b2aa897831a47e9ffed5c4b
Yan Wittmann <order@yanwittmann.de>
2021-06-25 20:00:02 +0200

    Fixed ApiTools sometimes generating different hashes for the same URL

--- test/yanwittmann/api/ApiToolsTest.java
+++ test/yanwittmann/api/ApiToolsTest.java
@@ -1,5 +1,6 @@
 package yanwittmann.api;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import yanwittmann.file.File;
 import yanwittmann.log.Log;
@@ -9,10 +10,21 @@ import java.net.URL;
 
 public class ApiToolsTest {
 
+    private static ApiTools apiTools;
+
+    @BeforeAll
+    public static void beforeAll() {
+        apiTools = new ApiTools(new File("res/apifiles"));
+    }
+
     @Test
     public void getDataTest() throws IOException {
-        ApiTools apiTools = new ApiTools(new File("res/apifiles"));
         new File(apiTools.get(new URL("https://www.w3.org/TR/PNG/iso_8859-1.txt"), ApiTools.FORCE_UPDATE)).readToArrayList().forEach(Log::info);
-        //new File(apiTools.get("https://github.com/Skyball2000/java-utility-classes", 3 * 1000)).readToArrayList().forEach(Log::info);
+        new File(apiTools.get("https://github.com/Skyball2000/java-utility-classes", 3 * 1000)).readToArrayList().forEach(Log::info);
+    }
+
+    @Test
+    public void clearDataTest() {
+        apiTools.clearStorageDirectory();
     }
 }
