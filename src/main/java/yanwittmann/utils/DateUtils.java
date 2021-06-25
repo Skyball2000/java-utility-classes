5e02f91eef2b0231e4102f56c852d4d849bb4605
Yan Wittmann <order@yanwittmann.de>
2021-06-25 21:01:09 +0200

    DateUtils added more functions Disabled Network tests

--- src/main/java/yanwittmann/utils/DateUtils.java
+++ src/main/java/yanwittmann/utils/DateUtils.java
@@ -4,23 +4,47 @@ import javafx.util.Pair;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Lots of date-related functions.<br>
+ * This class has been written by <a href="http://yanwittmann.de">Yan Wittmann</a>.
+ *
+ * @author Yan Wittmann
+ */
 public class DateUtils {
 
+    /**
+     * Will parse a given string into a <code>Date</code> by creating a <code>SimpleDateFormat</code> from the format.<br>
+     * The <code>SimpleDateFormat</code> locale will be set to <code>Locale.ENGLISH</code>.
+     *
+     * @return The parsed <code>Date</code>
+     * @throws ParseException If the string could not be parsed
+     */
     public static Date parseString(String stringDate, String format) throws ParseException {
         return new SimpleDateFormat(format, Locale.ENGLISH).parse(stringDate);
     }
 
+    /**
+     * Will parse a given string into a <code>Date</code> using the <code>SimpleDateFormat</code>.<br>
+     *
+     * @return The parsed <code>Date</code>
+     * @throws ParseException If the string could not be parsed
+     */
     public static Date parseString(String stringDate, SimpleDateFormat format) throws ParseException {
         return format.parse(stringDate);
     }
 
+    /**
+     * This is a powerful function that will attempt to read a string into a <code>Date</code> by guessing its format.<br>
+     * Although a lot of different formats are supported, not every string is currently detected correctly.
+     *
+     * @param stringDate The string to parse into a <code>Date</code>
+     * @return The parsed date
+     * @throws ParseException If the string could not be parsed
+     */
     public static Date parseString(String stringDate) throws ParseException {
 
         // normalize input string
@@ -28,8 +52,6 @@ public class DateUtils {
                 .replace(".", ":").replace(",", ":")
                 .replaceAll(" ?/ ?", "/").replace("- ", "-")
                 .replaceAll("(?<! )-", ":").trim();
-
-        System.out.println("checking input string: " + stringDate);
 
         StringBuilder parsableDate = new StringBuilder();
         StringBuilder parsableDateFormat = new StringBuilder();
@@ -55,7 +77,6 @@ public class DateUtils {
             Matcher matcher = knownString.getKey().matcher(stringDate);
             if (matcher.find()) {
                 stringDate = stringDate.replace(matcher.group(), "").trim();
-                System.out.println(knownString.getKey() + " -- " + knownString.getValue() + " -- " + matcher.group() + " -- " + stringDate);
                 parsableDate.append(" ").append(matcher.group());
                 parsableDateFormat.append(" ").append(knownString.getValue());
             }
@@ -90,7 +111,6 @@ public class DateUtils {
         parsableDate = new StringBuilder(parsableDate.toString().trim());
         parsableDateFormat = new StringBuilder(parsableDateFormat.toString().trim());
 
-        System.out.println("Pattern used to decode date (" + parsableDate + "): " + parsableDateFormat);
         return new SimpleDateFormat(parsableDateFormat.toString(), Locale.ENGLISH).parse(parsableDate.toString());
 
     }
@@ -99,7 +119,7 @@ public class DateUtils {
         for (Pair<Pattern, SimpleDateFormat> pattern : patterns) {
             Matcher matcher = pattern.getKey().matcher(stringDate);
             if (matcher.find()) {
-                System.out.println(pattern.getKey() + " -- " + pattern.getValue().toPattern() + " -- " + matcher.group());
+                //System.out.println(pattern.getKey() + " -- " + pattern.getValue().toPattern() + " -- " + matcher.group());
                 return new Pair<>(pattern.getValue(), matcher.group());
             }
         }
@@ -140,14 +160,116 @@ public class DateUtils {
             new Pair<>(Pattern.compile("" + NEW_DATE_PART_BEGINNING_PATTERN + DATE_AD_BC_PATTERN), "G"),
             new Pair<>(Pattern.compile("" + NEW_DATE_PART_BEGINNING_PATTERN + DATE_MONTH_STRING_PATTERN), "MMM"),
             new Pair<>(Pattern.compile("" + NEW_DATE_PART_BEGINNING_PATTERN + DATE_DAY_OF_WEEK_STRING_PATTERN), "EEE"),
-            new Pair<>(Pattern.compile(NEW_DATE_PART_BEGINNING_PATTERN + "'\\d{2}"), "''yy")
+            new Pair<>(Pattern.compile(NEW_DATE_PART_BEGINNING_PATTERN + "'" + DATE_SHORT_YEAR_PATTERN), "''yy")
     );
 
+    /**
+     * Will format a given date by creating a <code>SimpleDateFormat</code> from the format.<br>
+     * The <code>SimpleDateFormat</code> locale will be set to <code>Locale.ENGLISH</code>.
+     *
+     * @return The formatted <code>Date</code>.
+     */
     public static String formatDate(Date date, String format) {
         return new SimpleDateFormat(format, Locale.ENGLISH).format(date);
     }
 
+    /**
+     * Will format a given date using the <code>SimpleDateFormat</code>.<br>
+     *
+     * @return The formatted <code>Date</code>.
+     */
     public static String formatDate(Date date, SimpleDateFormat format) {
         return format.format(date);
+    }
+
+    /**
+     * Converts a <code>Date</code> into a <code>Calendar</code>.
+     */
+    public static Calendar toCalendar(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.clear();
+        calendar.setTime(date);
+        return calendar;
+    }
+
+    /**
+     * Converts a <code>Date</code> into a <code>Calendar</code> using a <code>TimeZone</code>.
+     */
+    public static Calendar toCalendar(Date date, TimeZone timeZone) {
+        Calendar calendar = toCalendar(date);
+        calendar.setTimeZone(timeZone);
+        return calendar;
+    }
+
+    /**
+     * Adds a certain amount of years to the <code>Date</code>.
+     */
+    public static Date addYears(Date date, int years) {
+        Calendar calendar = toCalendar(date);
+        calendar.add(Calendar.YEAR, years);
+        return calendar.getTime();
+    }
+
+    /**
+     * Adds a certain amount of months to the <code>Date</code>.
+     */
+    public static Date addMonths(Date date, int months) {
+        Calendar calendar = toCalendar(date);
+        calendar.add(Calendar.MONTH, months);
+        return calendar.getTime();
+    }
+
+    /**
+     * Adds a certain amount of days to the <code>Date</code>.
+     */
+    public static Date addDays(Date date, int days) {
+        Calendar calendar = toCalendar(date);
+        calendar.add(Calendar.DAY_OF_YEAR, days);
+        return calendar.getTime();
+    }
+
+
+    /**
+     * Adds a certain amount of hours to the <code>Date</code>.
+     */
+    public static Date addHours(Date date, int hours) {
+        Calendar calendar = toCalendar(date);
+        calendar.add(Calendar.HOUR_OF_DAY, hours);
+        return calendar.getTime();
+    }
+
+    /**
+     * Adds a certain amount of minutes to the <code>Date</code>.
+     */
+    public static Date addMinutes(Date date, int minutes) {
+        Calendar calendar = toCalendar(date);
+        calendar.add(Calendar.MINUTE, minutes);
+        return calendar.getTime();
+    }
+
+    /**
+     * Adds a certain amount of seconds to the <code>Date</code>.
+     */
+    public static Date addSeconds(Date date, int seconds) {
+        Calendar calendar = toCalendar(date);
+        calendar.add(Calendar.SECOND, seconds);
+        return calendar.getTime();
+    }
+
+    /**
+     * Adds a certain amount of milliseconds to the <code>Date</code>.
+     */
+    public static Date addMillis(Date date, int millis) {
+        Calendar calendar = toCalendar(date);
+        calendar.add(Calendar.MILLISECOND, millis);
+        return calendar.getTime();
+    }
+
+    public static long elapsedMillis(Date before, Date after) {
+        return elapsedMillis(toCalendar(before), toCalendar(after));
+    }
+
+    public static long elapsedMillis(Calendar before, Calendar after) {
+        return (after.getTimeInMillis() - before.getTimeInMillis());
     }
 }
--- test/yanwittmann/utils/DateUtilsTest.java
+++ test/yanwittmann/utils/DateUtilsTest.java
@@ -5,13 +5,15 @@ import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 
 public class DateUtilsTest {
 
-    @Test
-    public void fromDateTest() throws ParseException {
+    private final static SimpleDateFormat CHECK_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss z", Locale.ENGLISH);
 
+    @Test
+    public void detectDateFormatTest() throws ParseException {
         checkStringFormatter("         13:23:59  ", "1970-01-01T13:23:59 CET");
         checkStringFormatter("05/07/2000 13-23-59-912 -   0400", "2000-07-05T19:23:59 CEST");
         checkStringFormatter("05/07-2000 13-23-59", "2000-07-05T13:23:59 CEST");
@@ -27,14 +29,36 @@ public class DateUtilsTest {
         checkStringFormatter("2021-06-24", "2021-06-24T00:00:00 CEST");
         checkStringFormatter("18.06.2021", "2021-06-18T00:00:00 CEST");
         checkStringFormatter("1/2/2000 01:23", "2000-02-01T01:23:00 CET");
-
     }
-
-    private final static SimpleDateFormat CHECK_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss z", Locale.ENGLISH);
 
     public void checkStringFormatter(String timestamp, String expected) throws ParseException {
         System.out.println("\n" + timestamp + " ==> " + expected);
         Assertions.assertEquals(expected, CHECK_FORMAT.format(DateUtils.parseString(timestamp)));
+    }
+
+    @Test
+    public void modifyDateTest() throws ParseException {
+        Date date = DateUtils.parseString("1/2/2000 01:23");
+        date = DateUtils.addDays(date, 40);
+        Assertions.assertEquals("Sun Mar 12 01:23:00 CET 2000", date.toString());
+        date = DateUtils.addMonths(date, 1);
+        Assertions.assertEquals("Wed Apr 12 01:23:00 CEST 2000", date.toString());
+        date = DateUtils.addYears(date, 4);
+        Assertions.assertEquals("Mon Apr 12 01:23:00 CEST 2004", date.toString());
+        date = DateUtils.addHours(date, 6);
+        Assertions.assertEquals("Mon Apr 12 07:23:00 CEST 2004", date.toString());
+        date = DateUtils.addMinutes(date, 110);
+        Assertions.assertEquals("Mon Apr 12 09:13:00 CEST 2004", date.toString());
+        date = DateUtils.addSeconds(date, 50);
+        Assertions.assertEquals("Mon Apr 12 09:13:50 CEST 2004", date.toString());
+        date = DateUtils.addMillis(date, 2500);
+        Assertions.assertEquals("Mon Apr 12 09:13:52 CEST 2004", date.toString());
+    }
+
+    @Test
+    public void timeDiffTest() throws ParseException {
+        Assertions.assertEquals(518, DateUtils.elapsedMillis(DateUtils.parseString("1/2/2000 01:23"), DateUtils.parseString("Wed, July 4, '01")) / (1000 * 60 * 60 * 24));
+        Assertions.assertEquals(883, DateUtils.elapsedMillis(DateUtils.parseString("1/2/2000 01:23"), DateUtils.parseString("Wed, July 4, '02")) / (1000 * 60 * 60 * 24));
     }
 
 }
