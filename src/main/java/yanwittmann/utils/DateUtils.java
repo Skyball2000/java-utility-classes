a68691b05281b6e83d5b605667cc3bf065577f00
Yan Wittmann <order@yanwittmann.de>
2021-06-26 08:40:01 +0200

    Added DateUtils functions for getting and setting parts of the date Enhanced the date parser to parse more different date formats

--- src/main/java/yanwittmann/utils/DateUtils.java
+++ src/main/java/yanwittmann/utils/DateUtils.java
@@ -10,6 +10,8 @@ import java.util.regex.Pattern;
 
 /**
  * Lots of date-related functions.<br>
+ * For more information on dates see <a href="https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html">here</a>
+ * and <a href="https://docs.oracle.com/javase/tutorial/i18n/format/simpleDateFormat.html">here</a>.<br>
  * This class has been written by <a href="http://yanwittmann.de">Yan Wittmann</a>.
  *
  * @author Yan Wittmann
@@ -37,15 +39,20 @@ public class DateUtils {
         return format.parse(stringDate);
     }
 
+    public static Date parseString(String stringDate) throws ParseException {
+        return parseString(stringDate, true);
+    }
+
     /**
      * This is a powerful function that will attempt to read a string into a <code>Date</code> by guessing its format.<br>
-     * Although a lot of different formats are supported, not every string is currently detected correctly.
+     * Although a lot of different formats are supported, not every string is currently detected correctly.<br>
+     * The matching order is the following: Year, Month, Day, Hour, Minute, Second, Millisecond.<br><br>
      *
      * @param stringDate The string to parse into a <code>Date</code>
      * @return The parsed date
      * @throws ParseException If the string could not be parsed
      */
-    public static Date parseString(String stringDate) throws ParseException {
+    public static Date parseString(String stringDate, boolean monthFirstThenDay) throws ParseException {
 
         // normalize input string
         stringDate = stringDate.replaceAll(" {2,}", " ").replace("/", ":")
@@ -57,7 +64,9 @@ public class DateUtils {
         StringBuilder parsableDateFormat = new StringBuilder();
 
         // extract day from input string
-        Pair<SimpleDateFormat, String> day = extractPartFromString(stringDate, DAY_PATTERNS);
+        Pair<SimpleDateFormat, String> day;
+        if (monthFirstThenDay) day = extractPartFromString(stringDate, DAY_PATTERNS_MONTH_FIRST);
+        else day = extractPartFromString(stringDate, DAY_PATTERNS_DAY_FIRST);
         if (day != null) {
             stringDate = stringDate.replace(day.getValue(), "").trim();
             parsableDate.append(" ").append(day.getValue());
@@ -77,34 +86,86 @@ public class DateUtils {
             Matcher matcher = knownString.getKey().matcher(stringDate);
             if (matcher.find()) {
                 stringDate = stringDate.replace(matcher.group(), "").trim();
+                //System.out.println(stringDate + " -- " + knownString.getValue() + " -- " + matcher.group());
                 parsableDate.append(" ").append(matcher.group());
                 parsableDateFormat.append(" ").append(knownString.getValue());
             }
         }
 
         // check if there is any not yet found data
-        if (!parsableDateFormat.toString().contains("M")) {
-            Matcher matcher = DATE_MONTH_PATTERN.matcher(stringDate);
-            if (matcher.find()) {
-                stringDate = stringDate.replaceFirst(matcher.group(), "").trim();
-                parsableDate.append(" ").append(matcher.group());
-                parsableDateFormat.append(" ").append("M");
+        if (monthFirstThenDay)
+            if (!parsableDateFormat.toString().contains("M")) {
+                Matcher matcher = DATE_MONTH_PATTERN.matcher(stringDate);
+                if (matcher.find()) {
+                    stringDate = stringDate.replaceFirst(matcher.group(), "").trim();
+                    //System.out.println(stringDate + " -- M -- " + matcher.group());
+                    parsableDate.append(" ").append(matcher.group());
+                    parsableDateFormat.append(" ").append("M");
+                }
             }
-        }
         if (!parsableDateFormat.toString().contains("d") && !parsableDateFormat.toString().contains("D")) {
             Matcher matcher = DATE_DAY_PATTERN.matcher(stringDate);
             if (matcher.find()) {
                 stringDate = stringDate.replaceFirst(matcher.group(), "").trim();
+                //System.out.println(stringDate + " -- d -- " + matcher.group());
                 parsableDate.append(" ").append(matcher.group());
                 parsableDateFormat.append(" ").append("d");
             }
         }
+        if (!monthFirstThenDay)
+            if (!parsableDateFormat.toString().contains("M")) {
+                Matcher matcher = DATE_MONTH_PATTERN.matcher(stringDate);
+                if (matcher.find()) {
+                    stringDate = stringDate.replaceFirst(matcher.group(), "").trim();
+                    //System.out.println(stringDate + " -- M -- " + matcher.group());
+                    parsableDate.append(" ").append(matcher.group());
+                    parsableDateFormat.append(" ").append("M");
+                }
+            }
         if (!parsableDateFormat.toString().contains("y")) {
             Matcher matcher = DATE_YEAR_PATTERN.matcher(stringDate);
             if (matcher.find()) {
                 stringDate = stringDate.replaceFirst(matcher.group(), "").trim();
+                //System.out.println(stringDate + " -- yyyy -- " + matcher.group());
                 parsableDate.append(" ").append(matcher.group());
                 parsableDateFormat.append(" ").append("yyyy");
+            }
+        }
+        if (!parsableDateFormat.toString().contains("H") && !parsableDateFormat.toString().contains("h")
+            && !parsableDateFormat.toString().contains("K") && !parsableDateFormat.toString().contains("k")) {
+            Matcher matcher = DATE_HOUR_PATTERN.matcher(stringDate);
+            if (matcher.find()) {
+                stringDate = stringDate.replaceFirst(matcher.group(), "").trim();
+                //System.out.println(stringDate + " -- H -- " + matcher.group());
+                parsableDate.append(" ").append(matcher.group());
+                parsableDateFormat.append(" ").append("H");
+            }
+        }
+        if (!parsableDateFormat.toString().contains("m")) {
+            Matcher matcher = DATE_MINUTE_PATTERN.matcher(stringDate);
+            if (matcher.find()) {
+                stringDate = stringDate.replaceFirst(matcher.group(), "").trim();
+                //System.out.println(stringDate + " -- m -- " + matcher.group());
+                parsableDate.append(" ").append(matcher.group());
+                parsableDateFormat.append(" ").append("m");
+            }
+        }
+        if (!parsableDateFormat.toString().contains("s")) {
+            Matcher matcher = DATE_SECOND_PATTERN.matcher(stringDate);
+            if (matcher.find()) {
+                stringDate = stringDate.replaceFirst(matcher.group(), "").trim();
+                //System.out.println(stringDate + " -- s -- " + matcher.group());
+                parsableDate.append(" ").append(matcher.group());
+                parsableDateFormat.append(" ").append("s");
+            }
+        }
+        if (!parsableDateFormat.toString().contains("S")) {
+            Matcher matcher = DATE_SECOND_PATTERN.matcher(stringDate);
+            if (matcher.find()) {
+                stringDate = stringDate.replaceFirst(matcher.group(), "").trim();
+                //System.out.println(stringDate + " -- S -- " + matcher.group());
+                parsableDate.append(" ").append(matcher.group());
+                parsableDateFormat.append(" ").append("S");
             }
         }
 
@@ -139,7 +200,7 @@ public class DateUtils {
     private final static Pattern DATE_MINUTE_PATTERN = Pattern.compile("\\d{1,2}");
     private final static Pattern DATE_SECOND_PATTERN = Pattern.compile("\\d{1,2}");
     private final static Pattern DATE_MILLISECONDS_PATTERN = Pattern.compile("\\d{3}");
-    private final static Pattern DATE_TIMEZONE_PATTERN = Pattern.compile("(?:[-+]\\d{1,4}|[A-Z]{3,4}|[A-Z ]+)");
+    private final static Pattern DATE_TIMEZONE_PATTERN = Pattern.compile("(?:[-+]\\d{1,4}|[A-Z]{3,4}|[A-Z ]{2,})");
     private final static Pattern DATE_SPLITTER = Pattern.compile(":");
 
     private final static List<Pair<Pattern, SimpleDateFormat>> TIME_PATTERNS = Arrays.asList(
@@ -149,11 +210,18 @@ public class DateUtils {
             new Pair<>(Pattern.compile("(?<= |^|T)" + DATE_HOUR_PATTERN + DATE_SPLITTER + DATE_MINUTE_PATTERN), new SimpleDateFormat("hh:mm", Locale.ENGLISH))
     );
 
-    private final static List<Pair<Pattern, SimpleDateFormat>> DAY_PATTERNS = Arrays.asList(
+    private final static List<Pair<Pattern, SimpleDateFormat>> DAY_PATTERNS_MONTH_FIRST = Arrays.asList(
+            new Pair<>(Pattern.compile("" + NEW_DATE_PART_BEGINNING_PATTERN + DATE_MONTH_STRING_PATTERN + DATE_SPLITTER + DATE_DAY_PATTERN + DATE_SPLITTER + DATE_YEAR_PATTERN), new SimpleDateFormat("MMM:dd:yyyy", Locale.ENGLISH)),
+            new Pair<>(Pattern.compile("" + NEW_DATE_PART_BEGINNING_PATTERN + DATE_DAY_PATTERN + DATE_SPLITTER + DATE_MONTH_STRING_PATTERN + DATE_SPLITTER + DATE_YEAR_PATTERN), new SimpleDateFormat("dd:MMM:yyyy", Locale.ENGLISH)),
+            new Pair<>(Pattern.compile("" + NEW_DATE_PART_BEGINNING_PATTERN + DATE_MONTH_PATTERN + DATE_SPLITTER + DATE_DAY_PATTERN + DATE_SPLITTER + DATE_YEAR_PATTERN), new SimpleDateFormat("MM:dd:yyyy", Locale.ENGLISH)),
+            new Pair<>(Pattern.compile("" + NEW_DATE_PART_BEGINNING_PATTERN + DATE_YEAR_PATTERN + DATE_SPLITTER + DATE_MONTH_PATTERN + DATE_SPLITTER + DATE_DAY_PATTERN), new SimpleDateFormat("yyyy:MM:dd", Locale.ENGLISH))
+    );
+
+    private final static List<Pair<Pattern, SimpleDateFormat>> DAY_PATTERNS_DAY_FIRST = Arrays.asList(
             new Pair<>(Pattern.compile("" + NEW_DATE_PART_BEGINNING_PATTERN + DATE_MONTH_STRING_PATTERN + DATE_SPLITTER + DATE_DAY_PATTERN + DATE_SPLITTER + DATE_YEAR_PATTERN), new SimpleDateFormat("MMM:dd:yyyy", Locale.ENGLISH)),
             new Pair<>(Pattern.compile("" + NEW_DATE_PART_BEGINNING_PATTERN + DATE_DAY_PATTERN + DATE_SPLITTER + DATE_MONTH_STRING_PATTERN + DATE_SPLITTER + DATE_YEAR_PATTERN), new SimpleDateFormat("dd:MMM:yyyy", Locale.ENGLISH)),
             new Pair<>(Pattern.compile("" + NEW_DATE_PART_BEGINNING_PATTERN + DATE_DAY_PATTERN + DATE_SPLITTER + DATE_MONTH_PATTERN + DATE_SPLITTER + DATE_YEAR_PATTERN), new SimpleDateFormat("dd:MM:yyyy", Locale.ENGLISH)),
-            new Pair<>(Pattern.compile("" + NEW_DATE_PART_BEGINNING_PATTERN + DATE_YEAR_PATTERN + DATE_SPLITTER + DATE_MONTH_PATTERN + DATE_SPLITTER + DATE_DAY_PATTERN), new SimpleDateFormat("yyyy:MM:dd", Locale.ENGLISH))
+            new Pair<>(Pattern.compile("" + NEW_DATE_PART_BEGINNING_PATTERN + DATE_YEAR_PATTERN + DATE_SPLITTER + DATE_DAY_PATTERN + DATE_SPLITTER + DATE_MONTH_PATTERN), new SimpleDateFormat("yyyy:dd:MM", Locale.ENGLISH))
     );
 
     private final static List<Pair<Pattern, String>> COMMON_DATE_PATTERNS = Arrays.asList(
@@ -265,11 +333,163 @@ public class DateUtils {
         return calendar.getTime();
     }
 
+    /**
+     * Sets the year of a <code>Date</code>.
+     */
+    public static Date setYear(Date date, int year) {
+        Calendar calendar = toCalendar(date);
+        calendar.set(Calendar.YEAR, year);
+        return calendar.getTime();
+    }
+
+    /**
+     * Sets the month of a <code>Date</code>.
+     */
+    public static Date setMonth(Date date, int month) {
+        Calendar calendar = toCalendar(date);
+        calendar.set(Calendar.MONTH, month);
+        return calendar.getTime();
+    }
+
+    /**
+     * Sets the day in a month of a <code>Date</code>.
+     */
+    public static Date setDayOfMonth(Date date, int day) {
+        Calendar calendar = toCalendar(date);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        return calendar.getTime();
+    }
+
+    /**
+     * Sets the day in a year of a <code>Date</code>.
+     */
+    public static Date setDayOfYear(Date date, int day) {
+        Calendar calendar = toCalendar(date);
+        calendar.set(Calendar.DAY_OF_YEAR, day);
+        return calendar.getTime();
+    }
+
+    /**
+     * Sets the day in a week of a <code>Date</code>.
+     */
+    public static Date setDayOfWeek(Date date, int day) {
+        Calendar calendar = toCalendar(date);
+        calendar.set(Calendar.DAY_OF_WEEK, day);
+        return calendar.getTime();
+    }
+
+    /**
+     * Sets the hour of a <code>Date</code>.
+     */
+    public static Date setHour(Date date, int hour) {
+        Calendar calendar = toCalendar(date);
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        return calendar.getTime();
+    }
+
+    /**
+     * Sets the minute of a <code>Date</code>.
+     */
+    public static Date setMinute(Date date, int minute) {
+        Calendar calendar = toCalendar(date);
+        calendar.set(Calendar.MINUTE, minute);
+        return calendar.getTime();
+    }
+
+    /**
+     * Sets the second of a <code>Date</code>.
+     */
+    public static Date setSecond(Date date, int second) {
+        Calendar calendar = toCalendar(date);
+        calendar.set(Calendar.SECOND, second);
+        return calendar.getTime();
+    }
+
+    /**
+     * Sets the millisecond of a <code>Date</code>.
+     */
+    public static Date setMillis(Date date, int millis) {
+        Calendar calendar = toCalendar(date);
+        calendar.set(Calendar.MILLISECOND, millis);
+        return calendar.getTime();
+    }
+
+    /**
+     * Calculates how many milliseconds lay between two <code>Date</code>s.
+     */
     public static long elapsedMillis(Date before, Date after) {
         return elapsedMillis(toCalendar(before), toCalendar(after));
     }
 
+    /**
+     * Calculates how many milliseconds lay between two <code>Calendar</code>s.
+     */
     public static long elapsedMillis(Calendar before, Calendar after) {
         return (after.getTimeInMillis() - before.getTimeInMillis());
     }
+
+    /**
+     * Returns the year a <code>Date</code> has.
+     */
+    public static int getYear(Date date) {
+        return toCalendar(date).get(Calendar.YEAR);
+    }
+
+    /**
+     * Returns the month a <code>Date</code> has.<br>
+     * <b>Months start at 0!</b>
+     */
+    public static int getMonth(Date date) {
+        return toCalendar(date).get(Calendar.MONTH);
+    }
+
+    /**
+     * Returns the day of week a <code>Date</code> has.
+     */
+    public static int getDayOfWeek(Date date) {
+        return toCalendar(date).get(Calendar.DAY_OF_WEEK);
+    }
+
+    /**
+     * Returns the day of month a <code>Date</code> has.
+     */
+    public static int getDayOfMonth(Date date) {
+        return toCalendar(date).get(Calendar.DAY_OF_MONTH);
+    }
+
+    /**
+     * Returns the day of year a <code>Date</code> has.
+     */
+    public static int getDayOfYear(Date date) {
+        return toCalendar(date).get(Calendar.DAY_OF_YEAR);
+    }
+
+    /**
+     * Returns the hour a <code>Date</code> has.
+     */
+    public static int getHour(Date date) {
+        return toCalendar(date).get(Calendar.HOUR_OF_DAY);
+    }
+
+    /**
+     * Returns the minute a <code>Date</code> has.
+     */
+    public static int getMinute(Date date) {
+        return toCalendar(date).get(Calendar.MINUTE);
+    }
+
+    /**
+     * Returns the second a <code>Date</code> has.
+     */
+    public static int getSeconds(Date date) {
+        return toCalendar(date).get(Calendar.SECOND);
+    }
+
+    /**
+     * Returns the millisecond a <code>Date</code> has.
+     */
+    public static int getMilliseconds(Date date) {
+        return toCalendar(date).get(Calendar.MILLISECOND);
+    }
+
 }
--- test/yanwittmann/utils/DateUtilsTest.java
+++ test/yanwittmann/utils/DateUtilsTest.java
@@ -14,26 +14,29 @@ public class DateUtilsTest {
 
     @Test
     public void detectDateFormatTest() throws ParseException {
-        checkStringFormatter("         13:23:59  ", "1970-01-01T13:23:59 CET");
-        checkStringFormatter("05/07/2000 13-23-59-912 -   0400", "2000-07-05T19:23:59 CEST");
-        checkStringFormatter("05/07-2000 13-23-59", "2000-07-05T13:23:59 CEST");
-        checkStringFormatter("2001.07.04 AD at 12:08:56 PDT", "2001-07-04T21:08:56 CEST");
-        checkStringFormatter("2015-11-19 13:19:24.000", "2015-11-19T13:19:24 CET");
-        checkStringFormatter("2015-11-19T13:19:24 CET", "2015-11-19T13:19:24 CET");
-        checkStringFormatter("Wed, July 4, '01", "2001-07-04T00:00:00 CEST");
-        checkStringFormatter("Wed,3.4, '02", "2002-03-04T00:00:00 CET");
-        checkStringFormatter("19/Nov/2015:13:19:24 CET", "2015-11-19T13:19:24 CET");
-        checkStringFormatter("19/11/2015 13:23:59:912 EST", "2015-11-19T19:23:59 CET");
-        checkStringFormatter("Thu Nov 19 13:21:11 2015", "2015-11-19T13:21:11 CET");
+        checkStringFormatter("         13:23:59  ", "1970-01-01T13:23:59 CET", true);
+        checkStringFormatter("05/07/2000 13-23-59-912 -   0400", "2000-05-07T19:23:59 CEST", true);
+        checkStringFormatter("05/07-2000 13-23-59", "2000-05-07T13:23:59 CEST", true);
+        checkStringFormatter("2001.07.04 AD at 12:08:56 PDT", "2001-07-04T21:08:56 CEST", true);
+        checkStringFormatter("2015-11-19 13:19:24.000", "2015-11-19T13:19:24 CET", true);
+        checkStringFormatter("2015-11-19T13:19:24 CET", "2015-11-19T13:19:24 CET", true);
+        checkStringFormatter("Wed, July 4, '01", "2001-07-04T00:00:00 CEST", true);
+        checkStringFormatter("Wed,3.4, '02", "2002-03-04T00:00:00 CET", true);
+        checkStringFormatter("19/Nov/2015:13:19:24 CET", "2015-11-19T13:19:24 CET", true);
+        checkStringFormatter("Nov/19/2015:13:19:24 CET", "2015-11-19T13:19:24 CET", true);
+        checkStringFormatter("11/19/2015 13:23:59:912 EST", "2015-11-19T19:23:59 CET", true);
+        checkStringFormatter("Thu Nov 19 13:21:11 2015", "2015-11-19T13:21:11 CET", true);
+        checkStringFormatter("13:21:11 Thu Nov 19 2015", "2015-11-19T13:21:11 CET", true);
 
-        checkStringFormatter("2021-06-24", "2021-06-24T00:00:00 CEST");
-        checkStringFormatter("18.06.2021", "2021-06-18T00:00:00 CEST");
-        checkStringFormatter("1/2/2000 01:23", "2000-02-01T01:23:00 CET");
+        checkStringFormatter("2021-06-24", "2021-06-24T00:00:00 CEST", true);
+        checkStringFormatter("18.06.2021", "2021-06-18T00:00:00 CEST", false);
+        checkStringFormatter("1/2/2000 01:23", "2000-02-01T01:23:00 CET", false);
+        checkStringFormatter("1 2 2000 01 23", "2000-02-01T01:23:00 CET", false);
     }
 
-    public void checkStringFormatter(String timestamp, String expected) throws ParseException {
+    public void checkStringFormatter(String timestamp, String expected, boolean monthFirst) throws ParseException {
         System.out.println("\n" + timestamp + " ==> " + expected);
-        Assertions.assertEquals(expected, CHECK_FORMAT.format(DateUtils.parseString(timestamp)));
+        Assertions.assertEquals(expected, CHECK_FORMAT.format(DateUtils.parseString(timestamp, monthFirst)));
     }
 
     @Test
@@ -53,6 +56,20 @@ public class DateUtilsTest {
         Assertions.assertEquals("Mon Apr 12 09:13:50 CEST 2004", date.toString());
         date = DateUtils.addMillis(date, 2500);
         Assertions.assertEquals("Mon Apr 12 09:13:52 CEST 2004", date.toString());
+    }
+
+    @Test
+    public void getDateDataTest() throws ParseException {
+        Date date = DateUtils.parseString("04:23 1/3/2000");
+
+        Assertions.assertEquals(2000, DateUtils.getYear(date));
+        Assertions.assertEquals(2, DateUtils.getMonth(date));
+        Assertions.assertEquals(1, DateUtils.getDayOfMonth(date));
+        Assertions.assertEquals(4, DateUtils.getDayOfWeek(date));
+        Assertions.assertEquals(61, DateUtils.getDayOfYear(date));
+        Assertions.assertEquals(4, DateUtils.getHour(date));
+        Assertions.assertEquals(23, DateUtils.getMinute(date));
+        Assertions.assertEquals(0, DateUtils.getMilliseconds(date));
     }
 
     @Test
